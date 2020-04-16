da62664016bb04230ab45b598a5b0df8c0d216cb
yu-san51 <nekonyan.yuki51@gmail.com>
2020-04-16 06:41:29 +0000

    [Fix] bugs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,10 +11,12 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
+    @book = Book.find(params[:id])
   end
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
   end
 
   # POST /books
@@ -24,6 +26,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -31,18 +34,20 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
-    else
-      render :edit
-    end
+    book = Book.find(params[:id])
+      if book.update(book_params)
+        redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+      else
+        @book = book
+        render :edit
+      end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
